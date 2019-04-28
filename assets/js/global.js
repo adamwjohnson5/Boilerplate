e85f97c4ddf8a98eef2896f0f87648268bc6b0b0
Adam Johnson <adamjohnson@apple.com>
2019-04-28 21:34:18 +1000

    Initial

---
+++ assets/js/global.js
@@ -0,0 +1,57 @@
+/* Global vars */
+
+var touchScreen = false;
+var rotateTimer;
+var resizeTimerGlobal;
+
+/* On DOM load */
+
+document.addEventListener('DOMContentLoaded', function() {
+// Detect mobile
+if ((navigator.userAgent.match(/iPhone|iPad|iPod/i)) || (navigator.userAgent.match(/Android/i))) {
+touchScreen = true;
+}
+// Mouse events
+
+// Set-up
+window.addEventListener('scroll', function() {
+let i = window.pageYOffset;
+});
+window.addEventListener('resize', function() {
+if (! window.touchScreen) {
+responsiveGlobal();
+}
+});
+responsiveGlobal();
+window.addEventListener('orientationchange', function() {
+clearTimeout(window.rotateTimer);
+window.rotateTimer = setTimeout(function() {
+location.reload(); // Refresh page
+}, 250);
+});
+start(); // Start app
+});
+
+/* Responsive */
+
+function responsiveGlobal() {
+let i = window.innerWidth;
+if (i < 768) { // Mobile
+
+} else { // DT
+
+}
+// On resize finish
+clearTimeout(window.resizeTimerGlobal);
+window.resizeTimerGlobal = setTimeout(function() {
+
+}, 250);
+}
+
+/* Start */
+
+function start() {
+
+}
+
+/* Global */
