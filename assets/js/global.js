cd79388ad9f9c416fd86ba19ec7ddfba616ea533
Adam Johnson <adamjohnson@apple.com>
2020-06-09 14:52:14 -0700

    Formatting

--- assets/js/global.js
+++ assets/js/global.js
@@ -11,27 +11,36 @@ var resizeTimerGlobal;
 document.addEventListener('DOMContentLoaded', () => {
     // Detect mobile
     if (navigator.userAgent.match(/iPhone|iPad|iPod/i) || navigator.userAgent.match(/Android/i)) {
-        touchScreen = true;
+        window.touchScreen = true;
     }
-    setEventsGlobal(); // Mouse and keyboard events
-    // Set-up
+    
+    // Mouse and keyboard
+    setEventsGlobal();
+    
+    // Scroll
     window.addEventListener('scroll', () => {
-        let i = window.pageYOffset;
+        //window.pageYOffset;
     });
+    
+    // Resize
     window.addEventListener('resize', () => {
         if (! window.touchScreen) {
             responsiveGlobal();
         }
     });
     responsiveGlobal();
+    
+    // Orientation change
     window.addEventListener('orientationchange', () => {
         clearTimeout(window.rotateTimer);
         window.rotateTimer = setTimeout(() => {
             location.reload(); // Refresh page
         }, 250);
     });
+    
+    // Start app
     setTimeout(() => {
-        start(); // Start app
+        start();
     }, 500); // Short delay
 });
 
@@ -43,6 +52,7 @@ function responsiveGlobal() {
     } else { // DT
 
     }
+    
     // On resize finish
     clearTimeout(window.resizeTimerGlobal);
     window.resizeTimerGlobal = setTimeout(() => {
