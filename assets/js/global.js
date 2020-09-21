b9b54153b19b1b6564b8353c4071574b6b88d385
Adam Johnson <adamjohnson@apple.com>
2020-09-21 10:49:45 -0700

    Formatting

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,55 +6,53 @@ var touchScreen = false;
 
 /* On DOM load */
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Detect mobile
-    window.addEventListener('touchstart', () => {
-        document.querySelector('body').classList.remove('no-touch');
-        window.touchScreen = !window.touchScreen;
-    });
-    
-    // Mouse and keyboard
-    setEventsGlobal();
-    
-    // Scroll
-    window.addEventListener('scroll', () => {
-        //window.pageYOffset;
-    });
-    
-    // Resize
-    window.addEventListener('resize', () => {
-        if (!window.touchScreen) {
-            responsiveGlobal();
-        }
-    });
-    
-    responsiveGlobal();
-    
-    setTimeout(() => {
-        start();
-    }, 100); // Short pause to make sure DOM ready
+document.addEventListener("DOMContentLoaded", () => {
+  // Detect mobile
+  window.addEventListener("touchstart", () => {
+    document.querySelector("body").classList.remove("no-touch");
+    window.touchScreen = !window.touchScreen; // Toggle
+  });
+
+  // Mouse and keyboard
+  setEventsGlobal();
+
+  // Scroll
+  window.addEventListener("scroll", () => {
+    //window.pageYOffset;
+  });
+
+  // Resize
+  window.addEventListener("resize", () => {
+    if (!window.touchScreen) {
+      responsiveGlobal();
+    }
+  });
+
+  responsiveGlobal();
+
+  setTimeout(() => {
+    start();
+  }, 100); // Short pause to make sure DOM ready
 });
 
 /* Responsive */
 
 function responsiveGlobal() {
-    if (window.innerWidth < 768) { // Mobile
-        document.querySelector('body').classList.add('mobile');
-    } else { // DT/Tablet
-        document.querySelector('body').classList.remove('mobile');
-    }
+  if (window.innerWidth < 768) {
+    // Mobile
+    document.querySelector("body").classList.add("mobile");
+  } else {
+    // DT/Tablet
+    document.querySelector("body").classList.remove("mobile");
+  }
 }
 
 /* Mouse and keyboard events */
 
-function setEventsGlobal() {
-
-}
+function setEventsGlobal() {}
 
 /* Start */
 
-function start() {
-
-}
+function start() {}
 
 /* Global */
