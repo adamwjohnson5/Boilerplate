3341da3b519fcfea952d559aef6a51bdf51d8ff6
Adam Johnson <adam@adamjohnson.org>
2020-10-20 13:34:23 -0700

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -22,29 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Resize
     window.addEventListener('resize', () => {
-        if (! window.touchScreen) {
-            responsiveGlobal();
-        }
-    });
 
-    responsiveGlobal();
+    });
 
     setTimeout(() => {
         start();
     }, 100); // Short pause to make sure DOM ready
 });
-
-/* Responsive */
-
-function responsiveGlobal() {
-    if (window.innerWidth < 768) {
-        // Mobile
-        document.querySelector('body').classList.add('mobile');
-    } else {
-        // DT/Tablet
-        document.querySelector('body').classList.remove('mobile');
-    }
-}
 
 /* Mouse and keyboard events */
 
